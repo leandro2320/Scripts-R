e6013110c7eb0b7191aa5a5a68c11ba8d4a5cf2b
leandro2320 <leandro2320@gmail.com>
2016-10-11 11:24:35 -0300

    Add files via upload

---
+++ 01-Histograma_1_variable.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+library(gridExtra)
+
+setwd("c:/users/lavellaneda/desktop")
+d<-read.csv("precision.txt",header=T,sep="\t",dec=",")
+
+x=d[,1]                           #ingresar posicion variable a dibujar
+titulo="Histograma Precision"     #cambiar titulo grafico
+ejex="Query"                      #titulo eje x
+ejey="Precision"                  #titulo eje y
+
+p<-ggplot(d, aes(x=x)) + 
+  geom_histogram(color="black", fill="dodgerblue1")
+p<-p+ggtitle(titulo)+xlab(ejex)+ylab(ejey)
+p
+
+
+
+#para ajduntar otro histograma.
+p2<-ggplot(d, aes(x=x)) + 
+  geom_histogram(color="black", fill="dodgerblue1")
+p2<-p+ggtitle(titulo)+xlab(ejex)+ylab(ejey)
+
+grid.arrange(p, p2, ncol = 2)
